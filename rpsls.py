5a5d07b9684baead72f58143c402bfa0357b5297
beautiful345 <85606419+beautiful345@users.noreply.github.com>
2021-06-09 15:30:03 +0800

    Add files via upload

---
+++ rpsls.py
@@ -0,0 +1,68 @@
+#coding:gbk
+"""
+第一个小项目：Rock-paper-scissors-lizard-Spock
+作者：张姣
+日期：2021/06/04
+"""
+
+import random
+# 0 - 石头
+# 1 - 史波克
+# 2 - 纸
+# 3 - 蜥蜴
+# 4 - 剪刀
+
+# 以下为完成游戏所需要用到的自定义函数
+
+def name_to_number(name):
+    if name=="石头":
+	    name=0
+    elif name=="史波克":
+	    name=1
+    elif name=="布":
+	    name=2
+    elif name=="蜥蜴":
+	    name=3
+    else:
+        name=4
+    return name 
+def number_to_name(number):
+    """
+    将整数 (0, 1, 2, 3, or 4)对应到游戏的不同对象
+    """
+    if number==0:
+	    return"石头"
+    elif number==1:
+	    return"史波克"
+    elif number==2:
+	    return"布"
+    elif number==3:
+	    return"蜥蜴"
+    elif number==4:
+	    return"剪刀"	
+    else:
+        return"错误数字"
+def rpsls(name):
+    """
+    用户玩家任意给出一个选择，根据RPSLS游戏规则，在屏幕上输出对应的结果
+
+    """
+    
+# 对程序进行测试
+rpsls
+#rpsls("lizard")
+print("--------")
+name=input("请输入您的选择:")
+print("--------")
+playerchoice=name_to_number(name)
+print("您的选择为:")
+computerchoice=random.randrange(0,4)
+print("计算机的选择为:"+number_to_name(computerchoice))
+diff=(playerchoice-computerchoice)%5
+if (diff==1)or(diff==2):
+	print("您赢了！")
+elif (diff==3)or(diff==4):
+	print("机器赢了")
+else:
+	print("您和计算机出的一样呢")
+
